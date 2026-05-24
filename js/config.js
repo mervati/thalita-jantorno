5ce0f85b1730675f4bcf4d3dd9189124b9514ea1
mervati <maahervati@gmail.com>
2026-05-24 20:42:09 -0300

    Trocar para chave anon legada do Supabase

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Substitua pelos seus dados do Supabase (Project Settings > API)
 const SUPABASE_URL = 'https://amzygktdxgdnkeghhqoc.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_rc-hCkk4C7HD86P6p4rbFQ_cGQoJmgM';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtenlna3RkeGdkbmtlZ2hocW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NDkyODksImV4cCI6MjA5NTIyNTI4OX0._SrgQ-bP3q2FkLHP3hK51EgW0PgPPejuQEHEOZTSAKE';
 
 const WHATSAPP_NUMBER = '5527997830227';
 
